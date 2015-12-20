d468cdfceeadd2e72f49b9be366abc20aec5f86d
014289985 <sofia.kansikas@gmail.com>
2015-12-20 23:07:27 +0200

    it begins

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="nookansi"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="muistilista"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
